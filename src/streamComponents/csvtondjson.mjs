302ca3c617c3c9c83c860296d600cd3a59c32853
Seola <549747168@qq.com>
2022-12-21 19:58:56 +0800

    complete test

--- src/streamComponents/csvtondjson.mjs
+++ src/streamComponents/csvtondjson.mjs
@@ -1,8 +1,13 @@
 import { Transform } from "node:stream";
 
+// windows line break
+const BREAK_LINE_SYMBOL = "\n";
+const INDEX_NOT_FOUND = -1;
+
 export default class CSVToNDJSON extends Transform {
   #delimeter = "";
   #headers = [];
+  #buffer = Buffer.alloc(0);
 
   constructor({ delimiter = ",", headers }) {
     super();
@@ -10,13 +15,63 @@ export default class CSVToNDJSON extends Transform {
     this.#headers = headers;
   }
 
+  *#updateBuffer(chunk) {
+    this.#buffer = Buffer.concat([this.#buffer, chunk]);
+    let breaklineIndex = 0;
+    while (breaklineIndex !== INDEX_NOT_FOUND) {
+      breaklineIndex = this.#buffer.indexOf(Buffer.from(BREAK_LINE_SYMBOL));
+      if (breaklineIndex === INDEX_NOT_FOUND) break;
+
+      const lineToProcessIndex = breaklineIndex + BREAK_LINE_SYMBOL.length;
+      const line = this.#buffer.subarray(0, lineToProcessIndex);
+      const lineData = line.toString();
+
+      this.#buffer = this.#buffer.subarray(lineToProcessIndex);
+
+      // empty line
+      if (lineData === BREAK_LINE_SYMBOL) continue;
+
+      const NDJSONLine = [];
+      const headers = Array.from(this.#headers);
+      for (const item of lineData.split(this.#delimeter)) {
+        const key = headers.shift();
+        const value = item.replace(BREAK_LINE_SYMBOL, "");
+
+        // for headers
+        if (key === value) break;
+
+        NDJSONLine.push(`"${key}":"${value}"`);
+      }
+
+      if (!NDJSONLine.length) continue;
+      const ndJSONData = NDJSONLine.join(",");
+
+      yield Buffer.from(
+        "{".concat(ndJSONData).concat("}").concat(BREAK_LINE_SYMBOL)
+      );
+    }
+  }
+
   _transform(chunk, encoding, cb) {
-    cb(null, chunk);
+    for (const item of this.#updateBuffer(chunk)) {
+      // push into readable
+      this.push(item);
+    }
+    return cb();
   }
 
   // when it finishes processing
   // this.push(null) on the readable side
   _final(cb) {
+    if (!this.#buffer.length) {
+      return cb();
+    }
+
+    // grab the remaining data
+    for (const item of this.#updateBuffer(Buffer.from(BREAK_LINE_SYMBOL))) {
+      this.push(item);
+    }
+
     cb();
   }
 }
--- test/streamComponents/csvtondjson.test.js
+++ test/streamComponents/csvtondjson.test.js
@@ -1,4 +1,4 @@
-import { describe, it, expect } from "@jest/globals";
+import { describe, it, expect, jest } from "@jest/globals";
 
 import CSVToNDJSON from "../../src/streamComponents/csvtondjson.mjs";
 
@@ -9,11 +9,65 @@ import CSVToNDJSON from "../../src/streamComponents/csvtondjson.mjs";
  */
 
 describe("CSV to NDJSON test suite", () => {
-  it.todo("given a csv string it should return a ndjson string");
-  it.todo(
-    "it should work with strings that doesn't contains breaklines at the end"
-  );
-  it.todo(
-    "it should work with files that has breaklines in the beginning of the string "
-  );
+  it("given a csv string it should return a ndjson string", () => {
+    const csvString = `id,name,age\n01,foo,20\n`;
+    const csvToJSON = new CSVToNDJSON({
+      delimiter: ",",
+      headers: ["id", "name", "age"],
+    });
+
+    const expected = JSON.stringify({ id: "01", name: "foo", age: "20" });
+
+    const fn = jest.fn();
+    csvToJSON.on("data", fn);
+    csvToJSON.write(csvString);
+    csvToJSON.end();
+
+    const [current] = fn.mock.lastCall;
+    expect(JSON.parse(current)).toStrictEqual(JSON.parse(expected));
+  });
+
+  // undefined is not iterable (cannot read property Symbol(Symbol.iterator))
+  // means: it never calls the function
+  it("it should work with strings that doesn't contains breaklines at the end", () => {
+    const csvString = `id,name,age\n01,foo,20`;
+    const csvToJSON = new CSVToNDJSON({
+      delimiter: ",",
+      headers: ["id", "name", "age"],
+    });
+
+    const expected = JSON.stringify({ id: "01", name: "foo", age: "20" });
+
+    const fn = jest.fn();
+    csvToJSON.on("data", fn);
+    csvToJSON.write(csvString);
+    csvToJSON.end();
+
+    const [current] = fn.mock.lastCall;
+    expect(JSON.parse(current)).toStrictEqual(JSON.parse(expected));
+  });
+
+  it("it should work with files that has breaklines in the beginning of the string", () => {
+    const csvString = `\n\nid,name,age\n\n\n01,foo,20\n02,bar,30\n03,baz,40\n\n\n`;
+    const csvToJSON = new CSVToNDJSON({
+      delimiter: ",",
+      headers: ["id", "name", "age"],
+    });
+
+    const expected = [
+      JSON.stringify({ id: "01", name: "foo", age: "20" }),
+      JSON.stringify({ id: "02", name: "bar", age: "30" }),
+      JSON.stringify({ id: "03", name: "baz", age: "40" }),
+    ];
+
+    const fn = jest.fn();
+    csvToJSON.on("data", fn);
+    csvToJSON.write(csvString);
+    csvToJSON.end();
+
+    const [first, second, three] = fn.mock.calls;
+    expect(JSON.parse(first)).toStrictEqual(JSON.parse(expected[0]));
+    expect(JSON.parse(second)).toStrictEqual(JSON.parse(expected[1]));
+    expect(JSON.parse(three)).toStrictEqual(JSON.parse(expected[2]));
+  });
 });
